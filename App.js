d9627614465bd508f86b8f8ed137e4cf497d1658
abhijeet naikare <abhijeet.naikare@gmail.com>
2020-01-29 14:22:29 +1100

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Jai Shri Mataji! Happy Diwali !</Text>
+      <Text>Jai Shri Mataji! </Text>
     </View>
   );
 }
