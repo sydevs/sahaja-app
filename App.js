2f5d0719c72f3e0b75a58e19ea49b82a6d253e02
pivotal <pivotal@pivotal.io>
2020-02-02 15:38:21 +1100

    removing message

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Jai Shri Mataji! </Text>
+      
     </View>
   );
 }
