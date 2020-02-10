db92562aba7005f4531369366e79b78c9becfb30
pivotal <pivotal@pivotal.io>
2020-02-10 13:35:52 +1100

    added video library

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React,{ Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { Video } from 'expo-av';
 
 export default class App extends Component {
 render() {
@@ -7,6 +8,16 @@ render() {
         <View style = {styles.container} >
           <Image source = {require('./assets/Background.png')}
            />
+           <Video
+  source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+  rate={1.0}
+  volume={1.0}
+  isMuted={false}
+  resizeMode="cover"
+  shouldPlay
+  isLooping
+  style={{ width: 300, height: 300 }}
+/>
         </View>
        );
     }
