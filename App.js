fcb4b2f9b82657701cb576523ae6683bfb42904f
Luan Nguyen <lihavim@gmail.com>
2020-03-14 11:11:26 +0700

    Adding audio player

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React,{ Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Video } from 'expo-av';
 import R from 'res/R';
+// import AudioPlayer from 'library/components/AudioPlayer/AudioPlayer';
+import MusicPlayer from 'library/components/AudioPlayer/MusicPlayer';
 
 export default class App extends Component {
 render() {
@@ -9,7 +11,7 @@ render() {
         <View style = {styles.container} >
           {/* <Image source = {R.images.Background}
            /> */}
-           <Video
+           {/* <Video
               source={{ uri: R.uris.bigBuckBunny }}
               rate={1.0}
               volume={1.0}
@@ -18,11 +20,15 @@ render() {
               shouldPlay
               isLooping
               style={{ width: 300, height: 300 }}
-            />
+        useNativeControls={true}
+            /> */}
+            {/* <AudioPlayer /> */}
+            <MusicPlayer />
         </View>
        );
     }
   }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -30,4 +36,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); 
