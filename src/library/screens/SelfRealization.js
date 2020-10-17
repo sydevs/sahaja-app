3e44b0e4b0fe02553cdb24f92be60c71d1d67ff7
Luan Nguyen <lihavim@gmail.com>
2020-10-17 16:00:21 +0700

    Fix video issue on ios

--- src/library/screens/SelfRealization.js
+++ src/library/screens/SelfRealization.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import {View , StyleSheet, Dimensions} from 'react-native'
 import { Video } from 'expo-av';
+import { Audio } from 'expo-av';
 import  {Analytics} from 'aws-amplify';
 
 const { width, height } = Dimensions.get('window')
@@ -12,7 +13,25 @@ const SelfRealization  = props => {
           console.log('video finished, going to home scren ...')
           props.navigation.navigate({ routeName:  'Home'}) 
         }
+
+        if (playbackStatus.isLoaded && !playbackStatus.isPlaying) {
+            
+        }
     };
+
+    const _handleVideoRef = async (component) => {
+        const playbackObject = component
+        if (playbackObject) {
+            await playbackObject.loadAsync({
+                uri: "https://player.vimeo.com/external/363242273.sd.mp4?s=945156121c0de1cdd461c63fd9bfdbcf0ce68ec5&amp;profile_id=165&amp;oauth2_token_id=1254753263",
+                shouldPlay: true
+            })
+            // todo: Trigger fullScreen without videoStack loading
+            //playbackObject.presentFullscreenPlayer();
+            playbackObject.playAsync()
+            //playbackObject.setOnPlaybackStatusUpdate(this.onPlaybackStatusUpdate);
+        }
+    }
     const [isMute,setMute] = useState(false)
 
     const didBlurSubscription = props.navigation.addListener(
@@ -28,21 +47,31 @@ const SelfRealization  = props => {
             Analytics.record({ name: "Self Realization!" })
         }
         logEvent();
+        Audio.setAudioModeAsync({
+            allowsRecordingIOS: false,
+            interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+            playsInSilentModeIOS: true,
+            interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+            shouldDuckAndroid: true,
+            staysActiveInBackground: true,
+            playThroughEarpieceAndroid: true
+        })
     },[]);
 
     return (
          <View style={styles.screen}>
              
             <Video
-                ref={this._handleVideoRef}
-                source={{ uri: 'https://player.vimeo.com/external/363242273.sd.mp4?s=945156121c0de1cdd461c63fd9bfdbcf0ce68ec5&amp;profile_id=165&amp;oauth2_token_id=1254753263' }}
+                ref={_handleVideoRef}
+                // source={{ uri: 'https://player.vimeo.com/external/363242273.sd.mp4?s=945156121c0de1cdd461c63fd9bfdbcf0ce68ec5&amp;profile_id=165&amp;oauth2_token_id=1254753263' }}
                 onPlaybackStatusUpdate={(playbackStatus) => _onPlaybackStatusUpdate(playbackStatus)}
-                //rate={1.0}
+                rate={1.0}
                 volume={1.0}
                 isMuted={isMute}
-                resizeMode="stretch"
-                shouldPlay={ true }
+                resizeMode="cover"
+                shouldPlay={ false }
                 useNativeControls
+                shouldCorrectPitch={true}
                 style={styles.video}
                 />
         </View>
